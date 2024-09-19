14bc8c3a036831374035e4641c76ed1f123170be
Gp32Prog66 <szojo7@hotmail.com>
2024-09-19 02:09:27 -0700

    Added content to the json and js files

---
+++ serviceWorker.js
@@ -0,0 +1,15 @@
+const PWAScratch = "PWA From Scratch"
+const assets = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/app.js",
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
